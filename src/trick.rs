dba7f3120b18d795a1734acdeb857e44f4c88054
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-10-27 01:34:35 -0700

    trick: remove ref + lifetime

--- src/trick.rs
+++ src/trick.rs
@@ -3,11 +3,10 @@ use std::collections::BTreeSet;
 use crate::hand::Hand;
 use crate::player::Player;
 
-pub struct Trick<'a> {
+pub struct Trick {
     pub hand: Hand,
     pub current_player_id: usize,
     pub passed_player_ids: BTreeSet<usize>,
-    players: &'a [Player; 4],
 }
 
 pub enum InvalidPlayedHand {
@@ -15,7 +14,7 @@ pub enum InvalidPlayedHand {
     NotHighEnough,
 }
 
-impl<'a> Trick<'a> {
+impl Trick {
     pub fn try_play_hand(&mut self, hand: Hand) -> Result<(), InvalidPlayedHand> {
         assert!(!self.passed_player_ids.contains(&self.current_player_id));
         if let Hand::Pass = hand {
@@ -31,8 +30,8 @@ impl<'a> Trick<'a> {
     fn is_same_type(previous: &Hand, attempted: &Hand) -> bool {
         match (previous, attempted) {
             (Hand::Lone(_), Hand::Lone(_)) => true,
-            (Hand::Pair(_), Hand::Pair(_)) => true,
-            (Hand::Trips(_), Hand::Trips(_)) => true,
+            (Hand::Pair(_, _), Hand::Pair(_, _)) => true,
+            (Hand::Trips(_, _, _), Hand::Trips(_, _, _)) => true,
             _ => false,
         }
     }
@@ -49,7 +48,10 @@ impl<'a> Trick<'a> {
         }
     }
 
-    fn next_player_id(current_player_id: usize, passed_player_ids: &BTreeSet<usize>) -> Option<usize> {
+    fn next_player_id(
+        current_player_id: usize,
+        passed_player_ids: &BTreeSet<usize>,
+    ) -> Option<usize> {
         assert!(current_player_id < 4);
         for i in 1..4 {
             let next_id = (current_player_id + i) % 4;
@@ -65,6 +67,7 @@ impl<'a> Trick<'a> {
 mod tests {
 
     use super::*;
+    use crate::card::Card;
 
     #[test]
     fn test_check_hand_is_not_playable() {
@@ -93,27 +96,27 @@ mod tests {
     #[test]
     fn test_next_player_id() {
         let current_player_id: usize = 0;
-        let passed_player_ids : BTreeSet<usize> = BTreeSet::new();
+        let passed_player_ids: BTreeSet<usize> = BTreeSet::new();
         let next_player_id = Trick::next_player_id(current_player_id, &passed_player_ids).unwrap();
         assert_eq!(next_player_id, 1);
 
         let current_player_id: usize = 3;
-        let passed_player_ids : BTreeSet<usize> = BTreeSet::new();
+        let passed_player_ids: BTreeSet<usize> = BTreeSet::new();
         let next_player_id = Trick::next_player_id(current_player_id, &passed_player_ids).unwrap();
         assert_eq!(next_player_id, 0);
 
         let current_player_id: usize = 0;
-        let passed_player_ids : BTreeSet<usize> = BTreeSet::from([1,2]);
+        let passed_player_ids: BTreeSet<usize> = BTreeSet::from([1, 2]);
         let next_player_id = Trick::next_player_id(current_player_id, &passed_player_ids).unwrap();
         assert_eq!(next_player_id, 3);
 
         let current_player_id: usize = 2;
-        let passed_player_ids : BTreeSet<usize> = BTreeSet::from([0,3]);
+        let passed_player_ids: BTreeSet<usize> = BTreeSet::from([0, 3]);
         let next_player_id = Trick::next_player_id(current_player_id, &passed_player_ids).unwrap();
         assert_eq!(next_player_id, 1);
 
         let current_player_id: usize = 3;
-        let passed_player_ids : BTreeSet<usize> = BTreeSet::from([0, 1,2]);
+        let passed_player_ids: BTreeSet<usize> = BTreeSet::from([0, 1, 2]);
         let next_player_id = Trick::next_player_id(current_player_id, &passed_player_ids);
         assert_eq!(next_player_id, None);
     }
