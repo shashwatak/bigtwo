c728742329466140b67595b51d9e38b258faa752
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-10-30 01:46:09 -0700

    main starts using some of our stufff

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ mod hand;
 mod player;
 mod trick;
 
+use crate::deck::Deck;
+use crate::hand::Hand;
+use crate::player::Player;
+use crate::trick::Trick;
+
 fn main() {
-    println!("Hello, world!");
+    let deck = Deck::new();
+    let players = [Player::default; 4];
+    let starting_player: usize = 0;
+    let starting_hand: Hand = "3C".parse().unwrap();
+    let trick = Trick::new(starting_hand, starting_player);
+    println!("{}", trick);
 }
