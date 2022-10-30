95f0991ca84dbff476ad14b0c3eb62b3586f2fc1
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-10-30 02:47:28 -0700

    Player implementing PlaysHands

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ mod trick;
 
 use crate::deck::Deck;
 use crate::hand::Hand;
-use crate::player::Player;
+use crate::player::PassingPlayer;
 use crate::trick::Trick;
 
 fn main() {
     let deck = Deck::new();
-    let players = [Player::default; 4];
+    let players = [PassingPlayer::default; 4];
     let starting_player: usize = 0;
     let starting_hand: Hand = "3C".parse().unwrap();
     let trick = Trick::new(starting_hand, starting_player);
--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,16 @@
-use crate::card::Card;
+use crate::{card::Card, hand::Hand};
 
 #[derive(Debug, Default)]
-pub struct Player {
+pub struct PassingPlayer {
     pub cards: Vec<Card>,
+}
+
+pub trait PlaysHands {
+    fn playHand() -> Hand;
+}
+
+impl PlaysHands for PassingPlayer {
+    fn playHand() -> Hand {
+        Hand::Pass
+    }
 }
