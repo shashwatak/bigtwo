c40609270cc101a79f5633aa0768528002d5fed2
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-11-01 00:38:08 -0700

    hoist make Vec<Card> from &str into test_util module

---
+++ src/test_util.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+pub mod tests {
+   
+    use crate::card::Card;
+
+    pub fn vec_card_from_str(input: &str) -> Vec<Card> {
+        input
+            .split(' ')
+            .map(|x| x.parse().unwrap())
+            .collect::<Vec<Card>>()
+    }
+}
+
