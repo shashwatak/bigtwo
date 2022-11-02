0e8634a0763c1125fc5d0a954735c4d29e32f216
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-11-02 03:38:49 -0700

    main: deal cards to players

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,41 @@ mod player;
 mod test_util;
 mod trick;
 
+use crate::card::THREE_OF_CLUBS;
 use crate::deck::Deck;
 use crate::hand::Hand;
 use crate::player::Player;
 use crate::trick::Trick;
 
 fn main() {
-    let deck = Deck::new();
-    let players = [Player::default; 4];
-    let starting_player: usize = 0;
-    let starting_hand: Hand = "3C".parse().unwrap();
-    let trick = Trick::new(starting_hand, starting_player);
+    let mut players = <[Player; 4]>::default();
+    deal_cards(&mut players, Deck::new());
+    for (index, player) in players.iter().enumerate() {
+        println!("Player {}: {}", index, player);
+    }
+    let starting_player_idx = find_player_with_three_of_clubs(&players);
+    let starting_player = &players[starting_player_idx];
+    let starting_hand = (starting_player.submit_hand)(&"".parse().unwrap(), &starting_player.cards);
+    let trick = Trick::new(starting_hand, starting_player_idx);
     println!("{}", trick);
+}
+
+fn deal_cards(players: &mut [Player; 4], mut deck: Deck) {
+    // TODO: shuffle the deck :3
+    let mut player_index: usize = 0;
+    while let Some(card) = deck.cards.pop() {
+        let index = player_index % 4;
+        players[index].cards.push(card);
+        player_index += 1;
+    }
+    assert_eq!(deck.cards.len(), 0);
+}
+
+fn find_player_with_three_of_clubs(players: &[Player; 4]) -> usize {
+    for (index, player) in players.iter().enumerate() {
+        if player.cards.contains(&THREE_OF_CLUBS) {
+            return index;
+        }
+    }
+    unreachable!();
 }
