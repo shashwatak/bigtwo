9899c46e922ddf4c145f6d6241443cf63d3d3967
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-11-02 04:08:03 -0700

    player: start game with 3C in a combo

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use crate::{card::Card, hand::Hand};
+use crate::{card::{Card, THREE_OF_CLUBS}, hand::Hand};
 
 pub struct Player {
     pub cards: Vec<Card>,
@@ -26,11 +26,41 @@ impl Display for Player {
     }
 }
 
+pub const USE_THREE_OF_CLUBS: fn(&Vec<Card>) -> Hand = |cards| {
+    assert_eq!(cards[0], THREE_OF_CLUBS);
+    
+    match cards[..] {
+        [a, b, c, ..] => {
+            if let Ok(trips) = Hand::try_trips(a,b,c) {
+                return trips;
+            } else if let Ok(pair) = Hand::try_pair(a, b) {
+                return pair;
+            } else {
+                return Hand::Lone(a)
+            }
+        }
+        _ => panic!("oop"),
+    }
+    
+};
 pub const PLAY_SMALLEST_SINGLE_OR_PASS: fn(&Hand, &Vec<Card>) -> Hand = |hand, cards| Hand::Pass;
 
 #[cfg(test)]
 mod tests {
 
+    use super::*;
+    use crate::test_util::tests::vec_card_from_str;
+
     #[test]
     fn test_play_smallest_single_or_pass() {}
+
+    fn test_use_three_of_clubs() {
+        let cards = vec_card_from_str("3C 4C 5D");
+        let hand = USE_THREE_OF_CLUBS(&cards);
+        assert!(matches!(hand, Hand::Lone(a) if a == THREE_OF_CLUBS));
+        // match hand {
+        //     Hand::Lone(a) if a == THREE_OF_CLUBS => (),
+        //     _ => assert!(false),
+        // }
+    }
 }
