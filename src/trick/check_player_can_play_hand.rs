b135badb9f74afcd01c296a3b25a55027c262921
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-11-06 23:17:39 -0800

    minor whitespace change

--- src/trick/check_player_can_play_hand.rs
+++ src/trick/check_player_can_play_hand.rs
@@ -25,6 +25,7 @@ pub fn check_player_can_play_hand(
     player: &Player,
     attempt: &Hand,
 ) -> Result<(), PlayHandError> {
+
     if !Hand::is_same_type(current, attempt) {
         return Err(PlayHandError::Match);
     }
@@ -38,6 +39,7 @@ pub fn check_player_can_play_hand(
     }
 
     Ok(())
+
 }
 
 #[cfg(test)]
