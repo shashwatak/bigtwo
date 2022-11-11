0cc2813b38fe831063a114aedb0e62b5fb9cc202
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-11-11 10:57:35 -0800

    Hand: the first 5-card hand, Straight

--- src/hand.rs
+++ src/hand.rs
@@ -20,6 +20,8 @@ pub enum Hand {
     Pair(Card, Card),
     /// aka Three-of-a-Kind
     Trips(Card, Card, Card),
+    /// 5 Cards with consecutive Rank
+    Straight(Card, Card, Card, Card, Card),
     /// No Hand, No Cards
     Pass,
 }
@@ -66,6 +68,8 @@ pub enum InvalidHandError {
     UnmatchedTrips,
     /// Incorrect number of cards (0, 4, 6+)
     WrongQuantity,
+    /// Not a valid Five Card Hand
+    NotAFiveCardHand,
 }
 
 impl From<InvalidHandError> for ParseHandError {
@@ -82,6 +86,7 @@ impl Hand {
             [a] => Ok(Hand::Lone(*a)),
             [a, b] => Ok(Hand::try_pair(*a, *b)?),
             [a, b, c] => Ok(Hand::try_trips(*a, *b, *c)?),
+            [a, b, c, d, e] => Ok(Hand::try_fiver(*a, *b, *c, *d, *e)?),
             _ => Err(ParseHandError::InvalidHand(InvalidHandError::WrongQuantity)),
         }
     }
@@ -105,6 +110,40 @@ impl Hand {
         } else {
             Err(InvalidHandError::UnmatchedTrips)
         }
+    }
+
+    /// Given five cards, return either a valid Hand or an error
+    pub fn try_fiver(
+        first: Card,
+        second: Card,
+        third: Card,
+        fourth: Card,
+        fifth: Card,
+    ) -> Result<Hand, InvalidHandError> {
+        assert!(fifth < fourth);
+        assert!(fourth < third);
+        assert!(third < second);
+        assert!(second < first);
+
+        if Hand::check_straight(&first, &second, &third, &fourth, &fifth) {
+            Ok(Hand::Straight(first, second, third, fourth, fifth))
+        } else {
+            Err(InvalidHandError::NotAFiveCardHand)
+        }
+    }
+
+    /// Returns true if the first through fifth are consecutive descending Rank.
+    fn check_straight(
+        first: &Card,
+        second: &Card,
+        third: &Card,
+        fourth: &Card,
+        fifth: &Card,
+    ) -> bool {
+        first.rank as usize == second.rank as usize + 1
+            && second.rank as usize == third.rank as usize + 1
+            && third.rank as usize == fourth.rank as usize + 1
+            && fourth.rank as usize == fifth.rank as usize + 1
     }
 
     /// Return an Error if this slice of Cards is incoherent.
@@ -153,13 +192,6 @@ impl FromStr for Hand {
 impl Hand {
     /// Returns true when both provided Hands are the same variant.
     pub fn is_same_type(previous: &Hand, attempted: &Hand) -> bool {
-        matches!(
-            (previous, attempted),
-            (_, Hand::Pass)
-                | (Hand::Lone(_), Hand::Lone(_))
-                | (Hand::Pair(_, _), Hand::Pair(_, _))
-                | (Hand::Trips(_, _, _), Hand::Trips(_, _, _))
-        )
         matches!(attempted, Hand::Pass) || previous.cards().len() == attempted.cards().len()
     }
 }
@@ -167,12 +199,15 @@ impl Hand {
 impl Index<usize> for Hand {
     type Output = Card;
 
-    #[inline(always)]
     fn index(&self, index: usize) -> &Self::Output {
         match self {
+            // This code is a bit tedious, but it is simple and it allows us to impl the Iterator
+            // and ExactSizeIterator traits.
+            // NOTE: If we tried to use the Hand iterator here, it would cause an infinite recursion.
             Hand::Lone(a) if index == 0 => a,
             Hand::Pair(a, b) if index < 2 => [a, b][index],
             Hand::Trips(a, b, c) if index < 3 => [a, b, c][index],
+            Hand::Straight(a, b, c, d, e) if index < 5 => [a, b, c, d, e][index],
             _ => panic!("index {index} is out of bounds!"),
         }
     }
@@ -199,6 +234,7 @@ impl<'a> Iterator for HandIterator<'a> {
             Hand::Lone(..) if idx == 0 => Some(&self.hand[idx]),
             Hand::Pair(..) if idx < 2 => Some(&self.hand[idx]),
             Hand::Trips(..) if idx < 3 => Some(&self.hand[idx]),
+            Hand::Straight(..) if idx < 5 => Some(&self.hand[idx]),
             _ => None,
         }
     }
@@ -208,6 +244,7 @@ impl<'a> Iterator for HandIterator<'a> {
             Hand::Lone(..) => (1 - self.index, Some(1 - self.index)),
             Hand::Pair(..) => (2 - self.index, Some(2 - self.index)),
             Hand::Trips(..) => (3 - self.index, Some(3 - self.index)),
+            Hand::Straight(..) => (5 - self.index, Some(5 - self.index)),
         }
     }
 }
@@ -258,11 +295,19 @@ mod tests {
                 InvalidHandError::UnmatchedTrips
             ))
         ));
+
+        let hand = "2S 2H 5S 4S 3D".to_string().parse::<Hand>();
+        assert!(matches!(
+            hand,
+            Err(ParseHandError::InvalidHand(
+                InvalidHandError::NotAFiveCardHand
+            ))
+        ));
     }
 
     #[test]
     fn test_good_hand_to_from_string() {
-        let good_hands = ["", "2S", "3D 3C", "KS KH KC"];
+        let good_hands = ["", "2S", "3D 3C", "KS KH KC", "8S 7D 6S 5C 4C"];
         for expected_hand in good_hands {
             let hand = expected_hand.to_string().parse::<Hand>();
             println!("{:?}", hand);
@@ -285,7 +330,7 @@ mod tests {
 
     #[test]
     fn test_hand_index_iterator() {
-        let hand: Hand = "2S 2H 2D".parse().unwrap();
+        let hand: Hand = "2S AS KS QS JC".parse().unwrap();
         assert_eq!(
             hand[0],
             Card {
@@ -296,15 +341,29 @@ mod tests {
         assert_eq!(
             hand[1],
             Card {
-                rank: Rank::Two,
-                suit: Suit::Hearts
+                rank: Rank::Ace,
+                suit: Suit::Spades
             }
         );
         assert_eq!(
             hand[2],
             Card {
-                rank: Rank::Two,
-                suit: Suit::Diamonds
+                rank: Rank::King,
+                suit: Suit::Spades
+            }
+        );
+        assert_eq!(
+            hand[3],
+            Card {
+                rank: Rank::Queen,
+                suit: Suit::Spades
+            }
+        );
+        assert_eq!(
+            hand[4],
+            Card {
+                rank: Rank::Jack,
+                suit: Suit::Clubs
             }
         );
 
